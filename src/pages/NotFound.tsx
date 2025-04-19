590864787506889cc6b99291e4b8c00e2b16a234
Raka <incudelta@gmail.com>
2025-04-19 21:45:13 +0700

    feat: Update NotFound page with custom styling and localized content

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import RobotNotFound from "@/assets/robot.webp"
 
 const NotFound = () => {
   const location = useLocation();
@@ -12,12 +13,16 @@ const NotFound = () => {
   }, [location.pathname]);
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 font-nunitosans">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">404</h1>
-        <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
+    <div className="min-h-{window.innerHeight} flex items-center justify-center bg-custom-gray/20 font-nunitosans">
+      <div className="text-center pt-12">
+        <img src={RobotNotFound} alt="404 Image" className="mx-auto mb-8 w-48 h-48" loading="lazy" />
+        <h1 className="text-4xl font-extrabold mb-4 text-dark-blue">Oops!</h1>
+        <p className="text-xl text-gray-600 mb-4">Halaman yang Anda cari tidak ditemukan.</p>
+        <p className="text-xl text-gray-600 mb-6">
+          Mungkin Anda salah mengetik alamat URL atau halaman tersebut belum dibuat.
+        </p>
         <a href="/" className="text-blue-500 hover:text-blue-700 underline">
-          Return to Home
+          Kembali ke Beranda
         </a>
       </div>
     </div>
@@ -25,3 +30,4 @@ const NotFound = () => {
 };
 
 export default NotFound;
+
