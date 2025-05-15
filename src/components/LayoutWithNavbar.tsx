d82479833651e6e8e2aa1dd5c36631d6dd18bf55
Raka <incudelta@gmail.com>
2025-05-15 17:01:06 +0700

    feat: add LayoutWithNavbar component with Navbar and Outlet

---
+++ src/components/LayoutWithNavbar.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+import Navbar from '@/components/Navbar'
+export function LayoutWithNavbar() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  )
+}
