e47e411d5d5df69a8ec0b87d02df552df6568ba8
Raka <incudelta@gmail.com>
2025-05-20 20:54:38 +0700

    fix: update job titles for team members

--- src/components/CardTeam.tsx
+++ src/components/CardTeam.tsx
@@ -11,10 +11,10 @@ const name = [
 
 const jobTitles = [
   'UI Designer',
-  'UX Designer',
+  'Researcher',
   'Web Analyst',
   'Developer',
-  'Documentation',
+  'UX Designer',
 ];
 
 const instagramLinks = [
